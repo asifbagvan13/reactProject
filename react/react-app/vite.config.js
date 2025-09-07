40a826abe199642a0a522da66a0f42e43cf5b91f
BAGVAN AASIF LALMOHMADBHAI <113820484+asifbagvan13@users.noreply.github.com>
2025-09-07 19:29:23 +0530

    Update vite.config.js

--- react/react-app/vite.config.js
+++ react/react-app/vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-    base: '/reactProject/react/',
+    base: '/reactProject/react/', 
 })
