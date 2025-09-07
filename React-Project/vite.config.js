bfbc3ce6fe187e8a46d887b53479b1c483120f99
BAGVAN AASIF LALMOHMADBHAI <113820484+asifbagvan13@users.noreply.github.com>
2025-09-07 23:54:39 +0530

    Update vite.config.js

--- React-Project/vite.config.js
+++ React-Project/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactProject/reactProject',
+  base: '/reactProject/react-Project',
 })
+
 
 
 
